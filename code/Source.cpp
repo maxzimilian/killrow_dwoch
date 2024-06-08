2c278f39ec33e71b4695bc509e1c9328c87052e9
Chaus1 <jan.chalupka@student.put.poznan.pl>
2024-06-08 14:22:24 +0200

    sciany

--- code/Source.cpp
+++ code/Source.cpp
@@ -114,8 +114,8 @@ int main() {
     std::vector<Platform> platforms;
     // tiles
     platforms.push_back(Platform(&tileTexture, sf::Vector2f(512.0f, 160.0f), sf::Vector2f(256.0f, 800.0f))); // dolna ściana
-    platforms.push_back(Platform(&tileTexture, sf::Vector2f(288.0f, 2000.0f), sf::Vector2f(0.0f, 500.0f))); //lewa 144 800  72 400 - stara
-    platforms.push_back(Platform(&tileTexture, sf::Vector2f(288.0f, 2000.0f), sf::Vector2f(512.0f, 400.0f))); //prawa
+    platforms.push_back(Platform(&tileTexture, sf::Vector2f(288.0f, 1000.0f), sf::Vector2f(0.0f, 500.0f))); //lewa 144 800  72 400 - stara
+    platforms.push_back(Platform(&tileTexture, sf::Vector2f(288.0f, 1000.0f), sf::Vector2f(512.0f, 400.0f))); //prawa
     platforms.push_back(Platform(&tileTexture, sf::Vector2f(32.0f, 128.0f), sf::Vector2f(160.0f, 704.0f)));
     platforms.push_back(Platform(&tileTexture, sf::Vector2f(32.0f, 32.0f), sf::Vector2f(208.0f, 656.0f)));
     platforms.push_back(Platform(&tileTexture, sf::Vector2f(32.0f, 32.0f), sf::Vector2f(288.0f, 704.0f)));
